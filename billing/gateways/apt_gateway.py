afc7ca752d060a600d30333d2a493bbb3b07fb38
Tom Christensen <tom@itmachines.net>
2013-01-13 05:56:39 -0700

    stubbing in the APT gateway

--- billing/gateways/apt_gateway.py
+++ billing/gateways/apt_gateway.py
@@ -1,5 +1,4 @@
-import urllib
-import urllib2
+import requests
 import datetime
 
 from django.conf import settings
@@ -11,40 +10,111 @@ from billing.utils.credit_card import InvalidCard, Visa,\
     MasterCard, Discover, AmericanExpress
 from billing.utils.xml_parser import parseString, nodeToDic
 
+API_VERSION = "XWeb3.3"
+TRANS_TYPES = {
+    'purchase': 'CreditSaleTransaction',
+    'authorize': 'CreditAuthTranscation',
+    'capture': 'CreditCaptureTransaction',
+    'credit': 'CreditReturnTransaction',
+    'void': 'CreditVoidTransaction',
+    'store': 'AliasCreateTransaction',
+    'unstore': 'AliasDeleteTransaction',
+    'recurring': 'RecurringCreateTransaction',
+}
+
 class APTGateway(Gateway):
     test_url = 'https://test.t3secure.net/x-chargeweb.dll'
     prod_url = 'https://gw.t3secure.net/x-chargeweb.dll'
 
+    supported_countries = ["US"]
+    default_currency = "USD"
+
+    supported_cardtypes = [Visa, MasterCard, AmericanExpress, Discover]
+    homepage_url = "http://www.acceleratedpay.com/"
+    display_name = "APT"
+
+    def __init__(self, options=None, *args, **kwargs):
+        merchant_settings = getattr(settings, "MERCHANT_SETTINGS")
+        if not merchant_settings or not (merchant_settings.get("apt") or options):
+            raise GatewayNotConfigured("The '%s' gateway is not correctly "
+                                       "configured." % self.display_name)
+
+        apt_settings = options or merchant_settings["apt"]
+        self.login = apt_settings["LOGIN_ID"]
+        self.password = apt_settings["TRANSACTION_KEY"]
+
+    def _request(self, data, transaction_type):
+        data['TransactionType'] = TRANS_TYPES[transaction_type]
+        r = requests.get(self.service_url, params=data)
+        status = "SUCCESS"
+        if r.response_code != 1:
+            status = "FAILURE"
+            transaction_was_unsuccessful.send(sender=self,
+                type=transaction_type,
+                response=r)
+        else:
+            transaction_was_successful.send(sender=self,
+                type=transaction_type,
+                response=r)
+        return {"status": status, "response": r}
+
+    def _header_data(self, transaction_type):
+        post = dict()
+
+        post['SpecVersion'] = API_VERSION
+        post['XWebID'] = self.login
+        post['AuthKey'] = self.password
+        post['Industry'] = "ECOMMERCE"
+        post['TrackCapabilities'] = "NONE"
+        post['PinCapabilities'] = "FALSE"
+        post['POSType'] = "ECR"
+        return post
+
+    @property
+    def service_url(self):
+        if self.test_mode:
+            return self.test_url
+        return self.prod_url
+
+
     def purchase(self, money, credit_card, options=None):
         """One go authorize and capture transaction"""
-        raise NotImplementedError
-
+        data = self._header_data('purchase')
+        self._req
     def authorize(self, money, credit_card, options=None):
         """Authorization for a future capture transaction"""
-        raise NotImplementedError
+        data = self._header_data('authorize')
 
     def capture(self, money, authorization, options=None):
         """Capture funds from a previously authorized transaction"""
-        raise NotImplementedError
+        data = self._header_data('capture')
+        data['TransactionID'] = authorization
 
     def void(self, identification, options=None):
         """Null/Blank/Delete a previous transaction"""
-        raise NotImplementedError
+        data = self._header_data('void')
+        data['TransactionID'] = identification
 
     def credit(self, money, identification, options=None):
         """Refund a previously 'settled' transaction"""
-        raise NotImplementedError
+        data = self._header_data('credit')
+        data['TransactionID'] = identification
 
     def recurring(self, money, creditcard, options=None):
         """Setup a recurring transaction"""
-        raise NotImplementedError
+        data = self._header_data('recurring')
+
+    def cancel_recurring(self, identification, options=None):
+        data = self._header_data('RecurringCancelTransaction')
+        data['RecurringPlanID'] = identification
 
     def store(self, creditcard, options=None):
         """Store the credit card and user profile information
         on the gateway for future use"""
-        raise NotImplementedError
+        data = self._header_data('store')
 
     def unstore(self, identification, options=None):
         """Delete the previously stored credit card and user
         profile information on the gateway"""
-        raise NotImplementedError
+        data = self._header_data('unstore')
+        data['Alias'] = identification
